17e26e9ee9301ae31acb64baa0e0d95eefcde15d
GNANCADJA Marc Philippe <marcgnancadja4@gmail.com>
2023-03-03 15:06:47 +0100

    Updating relative paths

--- js/navbar.js
+++ js/navbar.js
@@ -4,12 +4,12 @@ class Navbar extends HTMLElement {
         super();
         this.innerHTML = `<header>
     <nav class="flex nav grow">
-        <img src="/assets/icons/logo_size1.jpg" alt="company logo" id="logo ">
+        <img src="./assets/icons/logo_size1.jpg" alt="company logo" id="logo ">
         <div class=" flex mobile-nav grow">
 
             <div class=" flex grow tablet-nav">
                 <ul class="flex mr-auto nav-links">
-                    <li class="link flex-item"> <a href="home.html"> Home </a></li>
+                    <li class="link flex-item"> <a href="index.html"> Home </a></li>
                     <li class="link flex-item"> <a href="about.html"> About Me </a></li>
                     <li class="link flex-item"> <a href="contact.html">Contact</a></li>
                 </ul>
@@ -22,14 +22,14 @@ class Navbar extends HTMLElement {
             </div>
 
             <ul class="flex ml-auto">
-                <a target = "_blank" href="https://twitter.com/MarcGnancadja" class="flex-item "><img src="/assets/icons/twitter.svg" alt="twitter logo"></a>
-                <a target = "_blank" href="https://www.instagram.com/marcgnancadja/" class="flex-item "><img src="/assets/icons/instagram.svg" alt=""></a>
-                <a target = "_blank" href="https://www.linkedin.com/in/marc-philippe-gnancadja/" class="flex-item "><img src="/assets/icons/Linkedin.svg" alt=""></a>
-                <a target = "_blank" href="https://github.com/Marc-Philippe-dev" class="flex-item "><img src="/assets/icons/GitHub.svg" alt=""></a>
+                <a target = "_blank" href="https://twitter.com/MarcGnancadja" class="flex-item "><img src="./assets/icons/twitter.svg" alt="twitter logo"></a>
+                <a target = "_blank" href="https://www.instagram.com/marcgnancadja/" class="flex-item "><img src="./assets/icons/instagram.svg" alt=""></a>
+                <a target = "_blank" href="https://www.linkedin.com/in/marc-philippe-gnancadja/" class="flex-item "><img src="./assets/icons/Linkedin.svg" alt=""></a>
+                <a target = "_blank" href="https://github.com/Marc-Philippe-dev" class="flex-item "><img src="./assets/icons/GitHub.svg" alt=""></a>
             </ul>
             <div class="vertical-divider none-divider2 hidden "></div>
         </div>
-        <img src="/assets/icons/hamburger_menu.svg" alt="hamburger_menu" class="hidden flex-item  hamburger-menu">
+        <img src="./assets/icons/hamburger_menu.svg" alt="hamburger_menu" class="hidden flex-item  hamburger-menu">
     </nav>
 </header>`
     }
